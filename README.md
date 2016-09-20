5598349c5e60e723638ac919a439bbcda3310615
aurelienshz <aurelien.schiltz@polyconseil.fr>
2016-09-20 16:49:13 +0200

    Redux WaitFor middleware

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
-redux-waitfor-middleware
+# redux-waitfor-middleware
+
+Lightweight Redux middleware to wait for specific actions to be dispatched. Very useful for testing purposes.
+
+## Installation
+
+```
+npm install redux-waitfor-middleware
+```
+
+## Usage
+
+Inject Waitfor Middleware in your modules (or React components) to start recording actions.
+Then, you can use it in tests to wait for a given action to be dispatched before a given delay.
+
+```javascript
+import createWaitForMiddleware from 'redux-waitfor-middleware';
+import {createStore} from 'redux';
+  
+const reducers = [
+  /* import your reducers */
+];
+const initialState = {
+  /* Initial state store */
+};
+
+const waitForMiddleware = createWaitForMiddleware();
+
+const store = applyMiddleware(waitForMiddleware)(createStore)(reducers, initialState);
+```
+
+The waitFor middleware has started recording ! You can then use its `waitFor` method to wait asynchronously for some actions to be dispatched :
+
+```javascript
+/**
+* Allows to await FOO_ACTION and BAR_ACTION actions to be dispatched :
+*/
+async function requiredActionsDispatched() {
+  await waitForMiddleware.waiFor([
+    'FOO_ACTION',
+    'BAR_ACTION'
+  ]);
+}
+```
+
+**Heads up !** If the actions have already been dispatched since the last call to `clean`, the method will immediately resolve with the payload of the actions found in its records.  
+
+This kind of method is very useful in cases where you want to test that a part of your app that might take some time to respond, for example your back-end, replies accordingly.  
+Check-list :
+- [x] you are able to fire a call to the back end in your test (you may be achieving this by simulating UI interaction)
+- [x] you are dispatching an action when a receiving a response from the back-end, whose payload is the content of the response
+
+```javascript
+describe('Potatoes list', () => {
+  it('should load a list of 5 potatoes', async () => {
+    // fire a request to the back-end, for example by simulating
+    // a click on the "load potaotes" button
+    
+    // This line will throw an Error if the POTATOES_RECEIVED action is not dispatched
+    // before 5000 ms, marking the test as failed :
+    const potaoesActions = await waitForMiddleware.waitFor(['POTATOES_RECEIVED'], 5000);
+    
+    // If the action is dispatched, you will now be able to make assertions on its payload :
+    
+    // waitFor resolves on an array of matching actions, but we only wait for one action to be dispatched, hence this assertion :
+    assert(potaoesActions.length === 1);
+    const potatoesList = potaoesActions[0].potatoesList;
+    assert(potatoesList.length === 5);
+  });
+});
+```
+
+## API
+
+The object returned by the `createWaitForMiddleware` method exposes the following methods :
+
+### `async waitFor(actions, timeout)`
+
+Wait asynchronously for `actions` to be dispatched.  
+**Heads up !** If the actions have already been dispatched since the last call to `clean`, the method will immediately resolve with the payload of the actions found in its records.   
+
+- `actions: Array<String>` : Actions types you are waiting for.
+- `timeout: number` : the timeout, in ms, after which waitFor will throw an Error. Default value : 2000.
+
+
+### `clean()`
+
+Cleans the recorded actions history. This allows to avoid side effects between several test cases (see "Heads up !" above).
---
+++ src/index.js
@@ -0,0 +1,115 @@
+// @flow
+
+export default function createWaitForMiddleware() {
+  /**
+   * Array of actions recorded by the middleware since the last clean() :
+   */
+  let records: Array<Object> = [];
+
+  /**
+   * List of Promises resolvers that are called each time an action is dispatched :
+   */
+  let waitedQueue: Array<{ resolver: (p: void) => void, id: number}> = [];
+
+  /**
+   * Keep track of emitted promises so we can give them an id and find them in the waited list :
+   * @type {number}
+   */
+  let emittedPromises = 0;
+
+  /**
+   * Remove a specific resolver from the waited queue :
+   * @param id int
+   */
+  const removeResolver = (id: number) => {
+    waitedQueue.splice(waitedQueue.findIndex(resolver => {
+      return resolver.id = id;
+    }), 1);
+  };
+
+  /**
+   * Match an array of actions against the list of recorded actions
+   * If any of the required actions is not found, will return null
+   */
+  const findRecorded = (actions: Array<string>): ? Array<Object> => {
+    // Find records in the history matching the waited actions :
+    const matches = [];
+
+    actions.forEach(actionType => {
+      const c = (records.find(record => {
+        return record.type && (record.type === actionType);
+      }));
+      if (c) matches.push(c);
+    });
+
+    // Checking every waited action was matched :
+    if (actions.every(type => matches.find(match => match.type === type))) {
+      return matches;
+    }
+    return null;
+  };
+
+  /**
+   * Redux middleware function :
+   */
+  const middleware = () => (next: (action: Object) => Object) => (action: Object) => {
+    records.push(action);
+    waitedQueue.forEach(waited => {
+      waited.resolver();
+    });
+    return next(action);
+  };
+
+  /**
+   * Clear the recorded actions array AND the waited actions queue :
+   */
+  middleware.clean = () => {
+    records = [];
+    waitedQueue = [];
+  };
+
+  /**
+   * Method exposed to allow waiting for a set of actions to be received.
+   * Its returned Promise :
+   * - resolves with the actions received (including payloads) if the required actions are found within timeout
+   * - rejects otherwise (timeout elapsed && actions not found)
+   * @param actions Array<string> Types of the waited actions
+   * @param timeout int Timeout before failing in ms
+   * @returns {Promise}
+   */
+  middleware.waitFor = async(actions: Array<string>, timeout: number = 2000) => {
+    emittedPromises++;
+    return new Promise((resolve, reject) => {
+      // Match recorded actions with those we are waiting for :
+      const matches = findRecorded(actions);
+
+      // Resolving if the actions have all happened already :
+      if (matches) { // XXX .length ?
+        resolve(actions);
+      } else {
+        // Setting timeout for rejection :
+        setTimeout(() => {
+          reject(new Error('Redux Recorder : One of the following actions was not dispatched before specified timeout was elapsed\n[' + actions.toString() + ']'));
+        }, timeout);
+
+        // Keeping track of the resolver's id to be able to remove it when it succeeds
+        const id = emittedPromises;
+        const resolver = () => {
+          const resolverMatches = findRecorded(actions);
+          if (resolverMatches) { // XXX .length ?
+            removeResolver(id);
+            resolve(resolverMatches); // Resolving with the received actions
+          }
+        };
+
+        // Pushing the resolver for this waiter in the queue read by the middleware :
+        waitedQueue.push({
+          resolver,
+          id,
+        });
+      }
+    });
+  };
+
+  return middleware;
+}
